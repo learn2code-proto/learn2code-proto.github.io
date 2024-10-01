62a5188e7d2f322cf133e39b5ef38a33ab8fb893
Mozzie <mozzie@mozzie.local>
2024-09-30 22:28:35 -0500

    added bash script

---
+++ build.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# ctw // 2024-09-30
+# builts HTML webpages
+
+PAGE_DIR='./pages'
+HOME_TITLE='Blocks'
+
+set -eu
+
+[ ! -d "$PAGE_DIR" ]             && echo 'pages directory not found' && exit 1
+[ ! -f "$PAGE_DIR/layout.html" ] && echo 'layout could not be found' && exit 2
+[ ! -f "$PAGE_DIR/core.html" ]   && echo 'core could not be found'   && exit 3
+
+LAYOUT="$(IFS=''; cat "$PAGE_DIR/layout.html")"
+
+for PAGE in $(find "$PAGE_DIR" -name '*.html' -print0 \
+                  | xargs -0 'realpath' \
+                  | grep -v "$PAGE_DIR/layout.html")
+do
+    if [[ "$PAGE" =~ .*'core.html'$ ]]
+    then
+        FILE_NAME='index.html'
+        TITLE="$HOME_TITLE"
+    else
+        FILE_NAME="$(basename "$PAGE")"
+        TITLE=( $(echo "$FILE_NAME" \
+                      | sed 's/.html//' \
+                      | tr '\-' ' ') )
+        TITLE="${TITLE[@]^}"
+    fi
+
+    CONTENT="${LAYOUT/'{{body}}'/"$(IFS=''; cat "$PAGE")"}"
+    CONTENT="${CONTENT/'{{title}}'/"$TITLE"}"
+
+    echo "$CONTENT" > "$FILE_NAME"
+done
+
+exit 0
