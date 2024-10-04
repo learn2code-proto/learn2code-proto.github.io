292ea89eb503d721b3c2b720e57f75a932521aeb
Mozzie <mozzie@mozzie.local>
2024-10-04 10:08:53 -0500

    documentation and placeholder text

---
+++ TODO.md
@@ -0,0 +1,24 @@
+# TODO
+
+## BLOCKLY
+
+- [ ] Implement variables
+- [ ] Use toolbox catagories
+- [ ] Create other blocks
+  - [ ] Create generators for those blocks
+  - [ ] Add code validation
+
+## TEXT EDITOR
+
+- [ ] Initial planning
+
+## STYLING
+
+- [ ] Background image
+- [ ] Improved navbar
+- [ ] Python syntax highlighting
+- [ ] Style Blockly
+  - [ ] Color code blocks
+  - [ ] Clearly denote where the block assembler starts and ends
+  - [ ] Fix widths of toolbox and assembler
+- [ ] Add option to fold code preview inward
--- scripts/core.js
+++ scripts/core.js
@@ -2,22 +2,27 @@ import "./blockly-core.js";
 
 console.log("prinsessasi on eri linnassa");
 
-const copyCodeButton = document.getElementById('save-code');
-copyCodeButton.addEventListener('click', copyCodeToClipboard);
+const copyCodeButton = document.getElementById('copy-code-button');
+const codeContainer = document.getElementById('code-container');
+const snackbar = document.getElementById('snackbar');
 
-// Copy all text in `code-container` to the clipboard and displays a message in
-// the snackbar.
-function copyCodeToClipboard() {
-    const codeContainer = document.getElementById('code-container');
-    const snackbar = document.getElementById('snackbar');
-
-    navigator.clipboard.writeText(codeContainer.innerText);
-
-    snackbar.innerText = 'Copied code to clipboard!';
+function displayOnSnackbar(text) {
+    // Displays the given text on the snackbar for a time.
+    snackbar.innerText = text;
     snackbar.className = 'view';
 
     setTimeout(function () {
         snackbar.className = snackbar.className.replace('view', '');
         snackbar.innerText = '';
     }, 4500);
+}
+
+copyCodeButton.addEventListener('click', copyCodeToClipboard);
+
+function copyCodeToClipboard() {
+    // Copy all text in `code-container` to the clipboard and displays a
+    // message in the snackbar.
+    navigator.clipboard.writeText(codeContainer.innerText);
+
+    displayOnSnackbar('Copied code to clipboard!');
 }
