1e2ca3407fbdf698b11b771e91ace67555de582b
Mihir Maruvada <71466097+Mihir-Maruvada@users.noreply.github.com>
2024-10-07 22:29:23 -0500

    Syntax Highlighting in Code Preview using Prism.js
    Edited index.html to import Prism.js and highlight initial(static) Code Preview
    Edited updateCode function in blocky-instance.js to dynamically update Code Preview syntax as new blocks are added/removed.

---
+++ blockly-instance.js
@@ -0,0 +1,139 @@
+import "./definitions.js"
+import "./generators.js"
+
+const toolbox = {
+    kind: 'categoryToolbox',
+    contents: [
+        {
+            kind: 'category',
+            name: 'Motors',
+            contents: [
+                {
+                    kind: 'block',
+                    type: 'smallmotor',
+                },
+                {
+                    kind: 'block',
+                    type: 'largemotor',
+                },
+            ],
+        },
+        {
+            kind: 'category',
+            name: 'Sensors',
+            contents: [
+                {
+                    kind: 'block',
+                    type: 'button',
+                },
+            ],
+        },
+        {
+            kind: 'category',
+            name: 'Time',
+            contents: [
+            ],
+        },
+        // {
+        //     kind: 'category',
+        //     name: 'Conditions',
+        //     contents: [
+        //         {
+        //             kind: 'block',
+        //             type: 'controls_if',
+        //         },
+        //         {
+        //             kind: 'block',
+        //             type: 'logic_compare',
+        //         },
+        //         {
+        //             kind: 'block',
+        //             type: 'logic_operation',
+        //         },
+        //         {
+        //             kind: 'block',
+        //             type: 'logic_negate',
+        //         },
+        //         {
+        //             kind: 'block',
+        //             type: 'logic_boolean',
+        //         },
+        //     ],
+        // },
+        {
+            kind: 'category',
+            name: 'Flow',
+            contents: [
+                {
+                    kind: 'block',
+                    type: 'controls_if',
+                },
+                {
+                    kind: 'block',
+                    type: 'controls_repeat_ext',
+                    inputs: {
+                        TIMES: {
+                            block: {
+                                type: 'math_number',
+                                fields: {
+                                    NUM: 10,
+                                },
+                            },
+                        },
+                    },
+                },
+                {
+                    kind: 'block',
+                    type: 'controls_whileUntil',
+                },
+            ],
+        },
+        // {
+        //     kind: 'category',
+        //     name: 'Math',
+        //     contents: [
+        //         {
+        //             kind: 'block',
+        //             type: 'math_number',
+        //             fields: {
+        //                 NUM: 123,
+        //             },
+        //         },
+        //         {
+        //             kind: 'block',
+        //             type: 'math_arithmetic',
+        //         },
+        //         {
+        //             kind: 'block',
+        //             type: 'math_single',
+        //         },
+        //     ],
+        // },
+    ],
+};
+
+var workspace = Blockly.inject('editor', {
+    toolbox: toolbox,
+});
+
+const supportedEvents = new Set([
+    Blockly.Events.BLOCK_CHANGE,
+    Blockly.Events.BLOCK_CREATE,
+    Blockly.Events.BLOCK_DELETE,
+    Blockly.Events.BLOCK_MOVE,
+]);
+
+function updateCode(event) {
+    // Updates the code displayed in `code-container`.
+    if (workspace.isDragging())
+        return;
+    if (!supportedEvents.has(event.type))
+        return;
+
+    const code = python.pythonGenerator.workspaceToCode(workspace);
+    const target = document.getElementById('code-container');
+
+    target.innerHTML = Prism.highlight('import make\n\n' + code, Prism.languages.python, 'python');
+}
+
+workspace.addChangeListener(updateCode);
