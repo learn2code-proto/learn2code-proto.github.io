c4df791f78f9e67bbb2ee5e92b24bfa7efb49e39
C Wiebe <mozzie@mozzie.local>
2024-10-12 22:56:48 -0500

    altered build.sh for better warning, and loaded scripts in pages rather than layout

--- build.sh
+++ build.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 
 # ctw // 2024-09-30
-# builds HTML webpages
-
-PAGE_DIR='./pages'
-HOME_TITLE='Block Coding Demo'
+# builds HTML webpages from a preset layout and a collection of individual
+# pages
 
 set -eu
+
+PAGE_DIR='./pages'
+HOME_TITLE='Block Coding Demo'  # for `index.html`
 
 [ ! -d "$PAGE_DIR" ]             && echo 'pages directory not found' && exit 1
 [ ! -f "$PAGE_DIR/layout.html" ] && echo 'layout could not be found' && exit 2
 [ ! -f "$PAGE_DIR/index.html" ]  && echo 'index could not be found'  && exit 3
+
+WARNING () {    # prints a warning to stdout intended to be a HTML header
+    cat << EOF
+<!-- --------------------------------------------------------------------------
+                           !! AUTO-GENERATED PAGE !!
+                             !! NOT SOURCE CODE !!
+
+Generated from a preset layout on $(date -Iseconds).
+For source files, look under \`$PAGE_DIR\` directory.
+Build with \`build.sh\`.
+--------------------------------------------------------------------------- -->
+EOF
+}
 
 LAYOUT="$(IFS=''; cat "$PAGE_DIR/layout.html")"
 
@@ -18,7 +32,7 @@ for PAGE in $(find "$PAGE_DIR" -name '*.html' -print0 \
                   | xargs -0 'realpath' \
                   | grep -v "$PAGE_DIR/layout.html")
 do
-    if [[ "$PAGE" =~ .*'index.html'$ ]]
+    if [[ "$PAGE" =~ .*'index.html'$ ]]  # `index.html` is a special case
     then
         FILE_NAME='index.html'
         TITLE="$HOME_TITLE"
@@ -28,12 +42,12 @@ do
         TITLE="${TITLE[@]^}"
     fi
 
-    echo '<!-- !! GENERATED PAGE !! NOT SOURCE CODE !! -->' > "$FILE_NAME"
+    PAGE_CONTENT="$(IFS=''; cat "$PAGE")"
+    GEN_CONTENT="${LAYOUT/'{{body}}'/"$PAGE_CONTENT"}"
+    GEN_CONTENT="${GEN_CONTENT/'{{title}}'/"$TITLE"}"
 
-    CONTENT="${LAYOUT/'{{body}}'/"$(IFS=''; cat "$PAGE")"}"
-    CONTENT="${CONTENT/'{{title}}'/"$TITLE"}"
-
-    echo "$CONTENT" >> "$FILE_NAME"
+    WARNING > "$FILE_NAME"
+    echo "$GEN_CONTENT" >> "$FILE_NAME"
 done
 
 exit 0
