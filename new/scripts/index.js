4be941396942a0a2a7cb2a12dfc37956599ee185
C Wiebe <cwiebe3@huskers.unl.edu>
2025-01-27 01:27:46 -0600

    begun migrating website

---
+++ new/scripts/index.js
@@ -0,0 +1,15 @@
+import "./blockly-instance/core.js";
+import { displayOnSnackbar } from './core.js';
+
+const copyCodeButton = document.getElementById('copy-code-button');
+const codeContainer = document.getElementById('code-container');
+
+copyCodeButton.addEventListener('click', copyCodeToClipboard);
+
+function copyCodeToClipboard() {
+  // Copy all text in `code-container` to the clipboard and display a
+  // message in the snackbar.
+  navigator.clipboard.writeText(codeContainer.innerText);
+
+  displayOnSnackbar('Copied code to clipboard!');
+}
