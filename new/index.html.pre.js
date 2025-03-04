8150fe21b2ff6220a4ac2bdf597ec2abef0eb5b8
C Wiebe <cwiebe3@huskers.unl.edu>
2025-03-03 22:41:45 -0600

    added download button and function

--- new/index.html.pre.js
+++ new/index.html.pre.js
@@ -4,6 +4,15 @@ function copyText(element) {
   window.navigator.clipboard.writeText(element.innerText);
 }
 
+function saveText(element) {
+  const encoding = btoa(element.innerText);
+  const dummyLink = document.createElement('a');
+
+  dummyLink.download = 'main.py';
+  dummyLink.href = 'data:text/python;base64,' + encoding;
+  dummyLink.dispatchEvent(new MouseEvent('click'));
+}
+
 function switchEditor() {
   document.getElementById('line-editor').toggleAttribute('data-closed');
   document.getElementById('block-editor').toggleAttribute('data-closed');
